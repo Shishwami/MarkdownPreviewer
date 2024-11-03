e46c2f649b03889cd116b9df81b8ec3ed4edb32d
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-03 18:43:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { marked } from 'marked';
 
+class MarkdownData {
+  static defaultMarkdown = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+}
 function App() {
-  const [markdown, setMarkdown] = useState("");
+
+  const [markdown, setMarkdown] = useState(MarkdownData.defaultMarkdown);
 
   const updateMarkdown = (event) => {
     setMarkdown(event.target.value);
@@ -13,7 +62,7 @@ function App() {
     <div className="App">
       <div id="wrapper">
         <div id="editor-container">
-          <textarea id="editor" onChange={updateMarkdown}></textarea>
+          <textarea id="editor" onChange={updateMarkdown} value={markdown}></textarea>
         </div>
         <div id="preview-container">
           <div id="preview">{markdown}</div>
