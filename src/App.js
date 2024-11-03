c682a0cd85319284dba3a5ec53ea78592e41ed23
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-03 18:36:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  const updateMarkdown = (event) => {
+    setMarkdown(event.target.value);
+  }
+
   return (
     <div className="App">
       <div id="wrapper">
         <div id="editor-container">
-          <textarea id="editor"></textarea>
+          <textarea id="editor" onChange={updateMarkdown}></textarea>
         </div>
         <div id="preview-container">
-          <div id="preview"></div>
+          <div id="preview">{markdown}</div>
         </div>
       </div>
     </div>
