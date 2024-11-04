3f081e969c6539fdb8f39b842b8fd9305894c97a
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-04 19:52:25 +0800

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,11 @@
-function Footer() {
-    
+function Footer({ children }) {
+    return (
+        <footer id="footer">
+            {children}
+            <p>by <a href="https://github.com/Shishwami">Shishwami</a></p>
+            <div></div>
+        </footer>
+    );
 }
 
 export default Footer;
