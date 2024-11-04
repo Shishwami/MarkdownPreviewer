abc790512a27d24a70650c2c9fc4f7fb2e4147f4
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-04 20:28:09 +0800

    Update Preview.js

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -2,7 +2,7 @@ import { marked } from 'marked';
 
 function Preview({ markdown }) {
     return (
-        <div id="preview-container">
+        <div id="preview-container" className="box">
             <h1>Preview</h1>
             <div id="preview" dangerouslySetInnerHTML={{ __html: marked(markdown) }}></div>
         </div>
