1bffaad97c958fa0b5a911277ac6e97cbe5ad111
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-05 21:23:10 +0800

    Update Preview.js

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
 import { marked } from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
 
 function Preview({ markdown }) {
+
+    useEffect(() => {
+        const blocks = document.querySelectorAll('pre code');
+        blocks.forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+    }, [markdown]); 
+
+    marked.setOptions({
+        highlight: (code, lang) => {
+            const language = lang || 'plaintext';
+            return hljs.highlight(language, code).value;
+        },
+    });
+
     return (
         <div id="preview-container" className="box">
             <h1 className="title">Preview</h1>
